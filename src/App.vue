165229ce6b45593249f319265347a7dc296e14c5
wuwenjia <wuwenjia@aegis-data.cn>
2022-08-19 17:41:14 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img src="./assets/logo.png" />
+  <hello-world
+      user-msg="Hello"
+      user-name="张三" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
